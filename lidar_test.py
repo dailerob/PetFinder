44c6714c66243a229ee94c24840c5a9b7af93e1b
Roberto Dailey <dailey.roberto@utexas.edu>
2017-12-02 23:35:35 -0600

    attempt to access softwareserial through nanpy

--- lidar_test.py
+++ lidar_test.py
@@ -23,6 +23,8 @@ i  = 0
 uart = [None] * 9
 HEADER = 0x59
 
+from nanpy import SoftwareSerial
+
 a.SoftwareSerial Serial1(2,3)
 
 a.Serial.begin(9600)
@@ -30,17 +32,17 @@ a.Serial1.begin(115200)
 
 
 while(True):
-	if a.Serial1.availible():
-		if a.Serial1.read()==HEADER:
-			uart[0] = HEADER
-			if a.Serial1.read()==HEADER:
-				uart[1] = HEADER
-				for i in range(2,9):
-					uart[i] = a.Serial1.read()
+    if a.Serial1.availible():
+        if a.Serial1.read()==HEADER:
+            uart[0] = HEADER
+            if a.Serial1.read()==HEADER:
+                uart[1] = HEADER
+                for i in range(2,9):
+                    uart[i] = a.Serial1.read()
 
-				#check=uart[0]+uart[1]+uart[2]+uart[3]+uart[4]+uart[5]+uart[6]+uart[7]
-				#if uart[8]==check and uart[8] == 0xff:
-				#	dist = uart[2]+uart[3]*256
-				#	print(str(dist))
+                #check=uart[0]+uart[1]+uart[2]+uart[3]+uart[4]+uart[5]+uart[6]+uart[7]
+                #if uart[8]==check and uart[8] == 0xff:
+                #   dist = uart[2]+uart[3]*256
+                #   print(str(dist))
 
 
