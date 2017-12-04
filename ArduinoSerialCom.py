ec8f0a27172b64a9aff291cf56a8bdb1f01cd4d8
Roberto Dailey <dailey.roberto@utexas.edu>
2017-12-03 19:29:28 -0600

    test for serial communication

---
+++ ArduinoSerialCom.py
@@ -0,0 +1,15 @@
+import serial
+time.sleep(3)
+
+ser = serial.Serial('COM3', baudrate = 9600, timeout=1)
+
+while 1:
+
+    arduinoData = ser.readline().decode('ascii')
+    print(arduinoData)
+    
+    
+
+
+
+    
