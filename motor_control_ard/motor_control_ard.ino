c9cca8c6713f6976f9282666c49b2bde38b505e7
Roberto Dailey <dailey.roberto@utexas.edu>
2017-12-05 13:13:18 -0600

    added commenting for getting the LIDAR data, currenty commented out

--- motor_control_ard/motor_control_ard.ino
+++ motor_control_ard/motor_control_ard.ino
@@ -1,3 +1,28 @@
+
+////////////////////////////////////////////////////LIDAR VARIABLES////////////////////////////////////////////
+
+//include software serial to allow serial communication with Lidar
+#include<SoftwareSerial.h>//softserialportheaderfile 
+//set the pins Lidar will use
+SoftwareSerial Serial1(2,3);
+
+//Lidar communication variables
+int dist;
+int strength; 
+int check;
+int i;
+int uart[9];
+const int HEADER =0x59;
+
+//Lidar Encoder
+const byte lidarEnPin = 13;
+//Lidar Motor
+//Handled by raspberry pi
+
+////////////////////////////////////////////////////LIDAR VARIABLES////////////////////////////////////////////
+
+////////////////////////////////////////////////////MOTOR VARIABLES///////////////////////////////////////////
+
 // connect motor controller pins to Arduino digital pins
 // motor one
 int enA = 10;
@@ -9,21 +34,81 @@ int in3 = 7;
 int in4 = 6;
 char dir = ' '; 
 
+////////////////////////////////////////////////////MOTOR VARIABLES///////////////////////////////////////////
+
+
+///////////////////////////////////////////////////SETUP//////////////////////////////////////////////////////
 void setup()
 {
-  // set all the motor control pins to outputs
-  Serial.begin(19200);
+  //Setup serial communication with the usb
+  Serial.begin(9600);
+  //Setup Serial communication with the Lidar
+  Serial1.begin(115200);
+
+  // Drive motor control pins 
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+
+  //Lidar motor Control pins
+  //Handled by raspberry pi now. 
+
+  //Lidar encoder pin 
+  pinMode(liEncoderPin, INPUT);
+
 }
 
 void loop()
 {
+  //Check external inputs 
+
+  /*
+  ///////////////////////////////////////////GET LIDAR DATA//////////////////////////////////////////////////
+  //Distance is saved under variable name dist
+
+  if(Serial1.available())
+  {
+    //Serial.println(Serial1.read());
+    if(Serial1.read()==HEADER) 
+    {
+
+      uart[0]=HEADER; 
+      if(Serial1.read()==HEADER)//determinedatapackageframeheader0x59 
+      {
+        uart[1]=HEADER; 
+        for(i=2;i<9;i++)//storedatatoarray 
+        {
+          uart[i] = Serial1.read();
+        }
+        check=uart[0]+uart[1]+uart[2]+uart[3]+uart[4]+uart[5]+uart[6]+uart[7]; 
+        if(uart[8]==(check&0xff))//checkthereceiveddataasperprotocols 
+        {
+          dist=uart[2]+uart[3]*256;//calculate distance value 
+          strength=uart[4]+uart[5]*256;//calculate signal strength value 
+        }
+      }
+    }
+  }
+
+  //Get Lidar encoder data
+  lidarState = digitalRead(pinNum);
+  lidarTime = millis()
+  ///////////////////////////////////////////GET LIDAR DATA//////////////////////////////////////////////////
+  */
+  
+  ///////////////////////////////////////////GET ENCODER DATA////////////////////////////////////////////////
+  //TODO
+
+  ///////////////////////////////////////////GET ENCODER DATA////////////////////////////////////////////////
+
+
+  ///////////////////////////////////////////READ INFO FROM PYTHON///////////////////////////////////////////
+  //Will become more complex as we can send more commands
   dir = Serial.read();
+  ///////////////////////////////////////////READ INFO FROM PYTHON///////////////////////////////////////////
   switch (dir) {
     case 'F':
       goForward();
