f36124f918c61b91447fa354d46f9bd170abca74
Roberto Dailey <dailey.roberto@utexas.edu>
2017-12-07 23:11:18 -0600

    tabs vs spaces, the final showdown

--- Python_Control.py
+++ Python_Control.py
@@ -12,8 +12,8 @@ from time import sleep
 ################################check if we want to run in a debug mode where we only read the serial output########
 print("Start command loop [c] or read serial output [r]")
 startResponse = input
-	if(command == "r"):
-		readSerial()
+  if(command == "r"):
+    readSerial()
 ################################check if we want to run in a debug mode where we only read the serial output########
 
 
@@ -49,41 +49,41 @@ motorTimePos = 0
 
 
 while direction != 'stop':
-	######################################################## MAIN LOOP FOR CONTROL ############################################
+  ######################################################## MAIN LOOP FOR CONTROL ############################################
 
-	if controlOn :
-		##logic for sending commands to the raspberry pi
-		sendMessage = ''
-		print("Enter [direction,time] to move forward/backwards or [turn direction,degrees]")
-		print("[Left = L, Right = R]")
-		print("[amount is in seconds]")
+  if controlOn :
+    ##logic for sending commands to the raspberry pi
+    sendMessage = ''
+    print("Enter [direction,time] to move forward/backwards or [turn direction,degrees]")
+    print("[Left = L, Right = R]")
+    print("[amount is in seconds]")
 
-		sendMessage = input()
-		direction = sendMessage[0]#get the first character of the inputed string 
-		wait = sendMessage[2]#get the first character of the inputed string 
-		output_serial.write(direction.encode())
-		startTime = lidarData
-
-
-	#######read###############
-	arduinoData = ser.readline().decode().strip()
-	if arduinoData[0] = "S" :
-		arduinoData = arduinoData.split(",")
-		lidarData = arduinoData[1]
-		lidarTime = arduinoData[2]
-		lidarEncoder = arduinoData[3]
-		motor1Pos = arduinoData[4]
-		motor2Pos = arduinoData[5]
-		motorTimePos = arduinoData[6]
-	#######read###############
-
-	if(lidarTime-Start < wait*1000): 
-		controlOn = False
-	else:
-		controlOn = True
+    sendMessage = input()
+    direction = sendMessage[0]#get the first character of the inputed string 
+    wait = sendMessage[2]#get the first character of the inputed string 
+    output_serial.write(direction.encode())
+    startTime = lidarData
 
 
-	######################################################## MAIN LOOP FOR CONTROL ############################################
+  #######read###############
+  arduinoData = ser.readline().decode().strip()
+  if arduinoData[0] = "S" :
+    arduinoData = arduinoData.split(",")
+    lidarData = arduinoData[1]
+    lidarTime = arduinoData[2]
+    lidarEncoder = arduinoData[3]
+    motor1Pos = arduinoData[4]
+    motor2Pos = arduinoData[5]
+    motorTimePos = arduinoData[6]
+  #######read###############
+
+  if(lidarTime-Start < wait*1000): 
+    controlOn = False
+  else:
+    controlOn = True
+
+
+  ######################################################## MAIN LOOP FOR CONTROL ############################################
 
 
 
@@ -92,47 +92,47 @@ disableLidar()
 
 
 def enableLidar():
-	for x in range (50):                          #execute loop for 50 times, x being incremented from 0 to 49.
-		p.ChangeDutyCycle(x)               #change duty cycle for varying the brightness of LED.
-		time.sleep(0.05) 
-	p.ChangeDutyCycle(50)
+  for x in range (50):                          #execute loop for 50 times, x being incremented from 0 to 49.
+    p.ChangeDutyCycle(x)               #change duty cycle for varying the brightness of LED.
+    time.sleep(0.05) 
+  p.ChangeDutyCycle(50)
 
 def disableLidar():
-	p.ChangeDutyCycle(0)
+  p.ChangeDutyCycle(0)
 
 def readSerial(): 
-	while(1):
-		arduinoData = ser.readline().decode().strip().split(",")
-		print('arduino Data' + str(arduinoData))
+  while(1):
+    arduinoData = ser.readline().decode().strip().split(",")
+    print('arduino Data' + str(arduinoData))
 
 '''
 def calcLidar(distance, encoderValue, lidarData):
-	
-	numRevsBeforeStart = 6
+  
+  numRevsBeforeStart = 6
 
 
-	prevEncoder = lidarData[0] The previous Encoder Value read
-	numCounts = lidarData[1] The number of counts since the encoder began counting 
-	pointsPerTab = lidarData[2] The number of points counted on each tab
-	currentTab = lidarData[3] The current tab index 
-	Distances = lidarData[4] the Distances calculated on each tab. 
-	Angles = lidarData[5] the angles for each tab
+  prevEncoder = lidarData[0] The previous Encoder Value read
+  numCounts = lidarData[1] The number of counts since the encoder began counting 
+  pointsPerTab = lidarData[2] The number of points counted on each tab
+  currentTab = lidarData[3] The current tab index 
+  Distances = lidarData[4] the Distances calculated on each tab. 
+  Angles = lidarData[5] the angles for each tab
 
-	if prevEncoder != encoderValue :
-		numCounts = numCounts+1
-	
-	#wait for lidar to spin up
-	if numcounts > numRevsBeforeStart*26:
-		#use 3 revs to determine correct tabs
-		if numcounts < (numRevsBeforeStart+3)*26:
-			if prevEncoder != encoderValue :
-				pointsPerTab[currentTab]= pointsPerTab[currentTab] + len(Distances[currentTab])
-				currentTab = currentTab + 1
+  if prevEncoder != encoderValue :
+    numCounts = numCounts+1
+  
+  #wait for lidar to spin up
+  if numcounts > numRevsBeforeStart*26:
+    #use 3 revs to determine correct tabs
+    if numcounts < (numRevsBeforeStart+3)*26:
+      if prevEncoder != encoderValue :
+        pointsPerTab[currentTab]= pointsPerTab[currentTab] + len(Distances[currentTab])
+        currentTab = currentTab + 1
 
-				Distances[]
+        Distances[]
 
 
-	prevEncoder
-	return (lidarData)
+  prevEncoder
+  return (lidarData)
 
 '''
